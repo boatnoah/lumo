107526c04934e5bff07a7deecd59dbef8d33d6e3
Noah <noahskim7@gmail.com>
2025-10-22 01:03:28 -0700

    created google added button with shadcn blocks

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,15 @@ import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-ste
 import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
-import OneTapComponent from "@/components/one-tap-component";
-
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <OneTapComponent />
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>testtttt</Link>
+              <Link href={"/"}>Lumo</Link>
               <div className="flex items-center gap-2">
                 <DeployButton />
               </div>
